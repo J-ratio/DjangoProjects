3e53d3aaae5cf9f633d5a906b6f9b78b16ed69cc
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-03-08 19:27:47 +0530

    added todo views
    -- index feature
    -- remove feature

--- ToDoWebapp/todo_site/todo_app/views.py
+++ ToDoWebapp/todo_site/todo_app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from .forms import TodoForm
+from .models import Todo
+
 
 # Create your views here.
+
+
+def index(request):
+    item_list = Todo.objects.order_by("-date")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_app')
+    form = TodoForm()
+
+    page = {
+        "forms": form,
+        "list": item_list,
+        "title": "TODO List"
+    }
+    return render(request, 'index.html', page)
+
+
+def remove(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, "item removed!")
+    return redirect('todo_app')
